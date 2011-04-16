1e29ead617e02e5613d3fcfa4efa5efae1e4dcb8
Mikael Silvén <mikael@silven.nu>
2011-04-16 16:33:41 +0200

    Initial commit, setup with swank

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject bomberman "0.0.1-SNAPSHOT"
+  :description "A bomberman ai tournament system"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [["swank-clojure" "1.2.1"]] )
---
+++ test/bomberman/test/core.clj
@@ -0,0 +1,6 @@
+(ns bomberman.test.core
+  (:use [bomberman.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
